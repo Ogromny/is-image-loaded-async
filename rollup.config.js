c63af75eac2be52906b876665cb3b8d7c6f8e283
Ogromny <ogromnycoding@gmail.com>
2018-02-18 12:52:49 +0100

    Release 1.0.0

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import resolve  from "rollup-plugin-node-resolve"
+import commonjs from "rollup-plugin-commonjs"
+import babel    from "rollup-plugin-babel"
+import uglify   from "rollup-plugin-uglify"
+import pkg      from "./package.json"
+
+export default [
+	{
+		input  : "src/is-image-loaded-async.js",
+		output : {
+			name  : "is-image-loaded-async",
+			file  : pkg.unpkg,
+			format: "umd"
+		},
+		plugins: [
+			resolve(),
+			commonjs(),
+			babel(),
+			uglify()
+		]
+	},
+	{
+		input : "src/is-image-loaded-async.js",
+		output: [
+			{file: pkg.main, format: "cjs"},
+			{file: pkg.module, format: "es"}
+		]
+	}
+]
---
+++ src/is-image-loaded-async.js
@@ -0,0 +1,24 @@
+export default function imgLoaded(el) {
+	if (el instanceof NodeList) {
+		if (el.length < 1) {
+			return Promise.reject(`Error: ${el} is empty.`)
+		}
+
+		return Promise.all(Array.prototype.map.call(el, img => imgLoaded(img)))
+	} else if (el instanceof Node) {
+		if (el.tagName.toLowerCase() !== "img") {
+			return Promise.reject(`Error: ${el} is not a img tag.`)
+		}
+
+		return new Promise((resolve, reject) => {
+			if (el.complete && el.naturalWidth) {
+				return resolve(`Success: ${el.src} loaded.`)
+			}
+
+			el.addEventListener("error", () => reject(`Error: ${el.src} cannot be loaded.`))
+			el.addEventListener("load", () => resolve(`Success: ${el.src} loaded.`))
+		})
+	} else {
+		return Promise.reject(`Error: ${el} is not a Node know type.`)
+	}
+}
